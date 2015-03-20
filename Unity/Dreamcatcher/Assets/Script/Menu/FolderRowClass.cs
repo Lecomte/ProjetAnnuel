89f0eb7b60b43225cc99855f84c7f94c20db1ad5
Lecomte <emerick.lecomte@gmail.com>
2015-03-20 10:18:08 +0100

    ImportMusic 75%

---
+++ Unity/Dreamcatcher/Assets/Script/Menu/FolderRowClass.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FolderRowClass : MonoBehaviour {
+    public Toggle toggle;
+    public InputField inputField;
+    public Button button;
+
+    public void changeEnableFolder(bool isActive)
+    {
+        isActive = this.toggle.isOn;
+        string name = this.inputField.text;
+        Debug.Log(name + " is active : " + isActive);
+    }
+
+    public void OpenClick()
+    {
+        Debug.Log("open folder : " + this.inputField.text);
+    }
+}
---
+++ Unity/Dreamcatcher/Assets/Script/Menu/ImportMusicScript.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using UnityEngine.UI;
+
+public class ImportMusicScript : MonoBehaviour {
+
+    public GameObject DirectoryPanel;
+    public RectTransform DirectoryPanelRecTransform;
+    public GameObject FilePanel;
+    public RectTransform FilePanelRecTransform;
+    public GameObject FolderRowPrefab;
+    public GameObject FileRowPrefab;
+
+    private string _folderSource;
+
+    private float _directoryPanelHeight;
+    private float _filePanelHeight;
+
+    private int _currentDirectoryPanelY;
+    private int _currentFilePanelY;
+
+    private GameObject[] _directoryRowInstance;
+    private GameObject[] _fileRowInstance;
+
+    void Start()
+    {
+        init();
+    }
+
+    public void init()
+    {
+        this._folderSource = Application.streamingAssetsPath;
+        //
+        this._directoryPanelHeight = DirectoryPanelRecTransform.sizeDelta.y;
+        this._filePanelHeight = FilePanelRecTransform.sizeDelta.y;
+        //
+        this._currentDirectoryPanelY = 0;
+        this._currentFilePanelY = 0;
+        //
+        loadAllRepository();
+    }
+
+    public void loadAllRepository()
+    {
+        string[] directory = Directory.GetDirectories(this._folderSource);
+        //
+        if (35 * directory.Length > DirectoryPanelRecTransform.sizeDelta.y)
+        {
+            DirectoryPanelRecTransform.sizeDelta += new Vector2(0, DirectoryPanelRecTransform.sizeDelta.y - (35 * directory.Length));
+            DirectoryPanelRecTransform.position -= new Vector3(0, DirectoryPanelRecTransform.sizeDelta.y / 2, 0);
+            //this._directoryPanelHeight = DirectoryPanelRecTransform.sizeDelta.y;
+        }
+        //
+        string name = "";
+        for (int i = 0; i < directory.Length; i++)
+        {
+            name = directory[i].Remove(0, this._folderSource.Length+1);
+            createDirectoryEntry(name);
+        }
+    }
+
+    private void createDirectoryEntry(string name)
+    {
+        _currentDirectoryPanelY -= 35;
+        //
+        Vector3 pos = new Vector3(210,_directoryPanelHeight + _currentDirectoryPanelY,DirectoryPanel.transform.position.z);
+        //
+        GameObject row = (GameObject)Instantiate(FolderRowPrefab);
+        row.GetComponentInChildren<InputField>().text = name;
+        row.transform.SetParent(DirectoryPanel.transform);
+        row.transform.position = pos;
+    }
+}

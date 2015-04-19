54784234795fd94c406b225e80e196e7231464e2
Benjamin Bourot <bourot.benjamin@gmail.com>
2015-04-19 12:37:44 +0200

    get Nearest entities

--- Unity/Dreamcatcher/Assets/Script/Event/ShootBulletScript.cs
+++ Unity/Dreamcatcher/Assets/Script/Event/ShootBulletScript.cs
@@ -4,15 +4,16 @@ using System.Collections.Generic;
 
 public class ShootBulletScript : MonoBehaviour {
 
-    BulletManager manager;
-    //SpawnManager spawnManager;
+    [SerializeField]
+    private BulletManager manager;
+    [SerializeField]
+    private SpawnManager spawnManager;
 
 	// Use this for initialization
     public void Shoot(float bulletDamageCoeff) 
     {
         // TODO get Nearest Enemy
-        //List<Collider> units =  spawnManager.GetActiveUnits();
-        List<Collider> units = new List<Collider>();
+        List<Collider> units =  spawnManager.GetActiveUnits();
         float minDist = float.MaxValue;
         float dist = 0f;
         Vector3 destination = Vector3.zero;

e96f4541fac300e78d1f99df47fbb455417072a7
Lecomte <emerick.lecomte@gmail.com>
2015-07-01 17:28:39 +0200

    Interne Spothlight Manager

---
+++ Unity/Dreamcatcher/Assets/Script/Ligth/LightClass.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightClass : MonoBehaviour {
+    public Light gLight;
+    public float MaxRedColor;
+    public float MaxGreenColor;
+    public float MaxBlueColor;
+    public int previousSign = 1;
+}
---
+++ Unity/Dreamcatcher/Assets/Script/Ligth/LigthManager.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LigthManager : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject Support;
+
+    [SerializeField]
+    private List<LightClass> LightArray;
+
+    [SerializeField]
+    private float RotationSpeed=1.0f;
+
+    //on pose x : cos
+    //z : sin
+
+    void Start()
+    {
+        DisposeLight();
+    }
+
+    public void DisposeLight()
+    {
+        float angleStep = 360 / LightArray.Count;
+        for (int i = 0; i < LightArray.Count;i++ )
+        {
+            LightArray[i].gLight.transform.localPosition = new Vector3(Mathf.Cos(i * angleStep * Mathf.Deg2Rad)/2, 0, Mathf.Sin(i * angleStep * Mathf.Deg2Rad)/2);
+        }
+    }
+
+    private void IntensityChange()
+    {
+        foreach(LightClass lightClass in LightArray)
+        {
+            if (Random.Range(0, 4) != 0) continue;
+            lightClass.gLight.color = Color.Lerp(lightClass.gLight.color,new Color(Random.Range(0.0f, lightClass.MaxRedColor / 255.0f),
+                Random.Range(0.0f, lightClass.MaxGreenColor / 255.0f),
+                Random.Range(0.0f, lightClass.MaxBlueColor / 255.0f)),Time.time);
+        }
+    }
+
+    float MaxXAngleRotation = 90;
+    float MinXAngleRotation = 60;
+
+    private void changeRotation()
+    {
+        foreach (LightClass CurrentLight in LightArray)
+        {
+            if (Random.Range(0, 4) == 0) continue;
+            if (CurrentLight.gLight.transform.localRotation.eulerAngles.x >= MaxXAngleRotation)
+            {
+                CurrentLight.previousSign = 1;
+            }
+            else if (CurrentLight.gLight.transform.localRotation.eulerAngles.x <= 60)
+            {
+                CurrentLight.previousSign = -1;
+            }
+            CurrentLight.gLight.transform.Rotate(CurrentLight.previousSign * (RotationSpeed / 2), 0, 0, Space.Self);
+        }
+    }
+
+    private void applyRandomChange()
+    {
+        Support.transform.Rotate(0, RotationSpeed,0,Space.Self);
+        changeRotation();
+        IntensityChange();
+    }
+
+
+    void FixedUpdate()
+    {
+        applyRandomChange();
+    }
+}
